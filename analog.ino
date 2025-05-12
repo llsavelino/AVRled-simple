a63a001f278710bf937d5cc7a15ba13cd14c999d
Lenilson A.S <savelino852@gmail.com>
2025-05-12 14:45:44 -0300

    Update analog.ino

--- analog.ino
+++ analog.ino
@@ -2,24 +2,33 @@
 // Macros para manipulação de bits:
 // - LOWST: Limpa o bit especificado no registrador - HIGST: Seta o bit especificado no registrador - OUTIN: Configura o pino como saída (equivalente a setar o bit no DDR)
 #define LOWST(pim, bim, reg) (reg &= ~(bim << pim))
+#define INOUT(pim, bim, reg) (reg &= ~(bim << pim))
 #define HIGST(pim, bim, reg) (reg |=  (bim << pim))
 #define OUTIN(pim, bim, reg) (reg |=  (bim << pim))
-#define INOUT(pim, bim, reg) (reg &= ~(bim << pim))
-#define ERRO(x, y, z) { digitalWrite(x, y); pinMode(x, z); Serial.print("Erro crítico desconhecido\n"); goto label; } 
+#define SWAPS(pim, bim, reg) (reg ^=  (bim << pim))
+#define CLEARL(lef, bim, reg) (reg >>= (bim << lef))
+#define CLEARR(rgh, int, reg) (reg <<= (int >> rgh))
+#define ERRO(x, y, z) do { digitalWrite(x, y); pinMode(x, z); goto label; } while(0x00) 
 extern              "C"  { void __asmFunc(void); };
 static unsigned char pwm0 = 0xA1;  // Valor inicial do PWM (161 em decimal)
 static bool state = 0x00;          // Estado atual do PWM (0 = baixo, 1 = alto)
 
 // Rotina de Interrupção do Timer2 (Overflow)
-ISR(TIMER2_OVF_vect) {
-  switch (state) {
+ISR(  TIMER2_OVF_vect  )
+{
+  switch (state) 
+  {
     #if defined(HIGST)
-    case true: TCNT2 = pwm0; // Reinicia o timer com valor pwm0
-      HIGST(PB4, 0x01, PORTB); /* Seta o pino PB4 (HIGH) */ state = false; break;
+    case true: state = (false >> 0); 
+      TCNT2 = pwm0; // Reinicia o timer com valor pwm0
+      HIGST(PB4, 0x01, PORTB); /* Seta o pino PB4 (HIGH) */ 
+      break;
     #endif
     #if defined(LOWST)
-    case false: TCNT2 = 0xFF -pwm0; // Reinicia o timer com valor complementar
-      LOWST(PB4, 0x01, PORTB); /* Limpa o pino PB4 (LOW) */ state = true; break;
+    case false: state = (true >> 0);
+      TCNT2 = 0xFF -pwm0; // Reinicia o timer com valor complementar
+      LOWST(PB4, 0x01, PORTB); /* Limpa o pino PB4 (LOW) */  
+      break; 
     #endif
     #if !defined(LOWST) || defined(HIGST)
     default: ERRO(12, LOW, INPUT_PULLUP); break;
@@ -27,8 +36,8 @@ ISR(TIMER2_OVF_vect) {
   } while (0x00) {  __asmFunc();  }
 }
 #ifndef brain_Main
-void setup() {
-  Serial.begin(9600);
+void setup(void) 
+{
   #if defined(OUTIN) && defined(LOWST)
   OUTIN(PB4, 0x01, DDRB); LOWST(PB4, 0x01, PORTB);
   #else
@@ -50,4 +59,4 @@ void setup() {
   TIMSK2 = 0x01;
 }
 #endif
-/* Loop principal (vazio, toda a lógica é feita na interrupção) */ void loop() { label: }
+/* Loop principal (vazio, toda a lógica é feita na interrupção) */ void loop(void) { label: }
